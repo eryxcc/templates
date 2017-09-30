3822126a4f3ba5b2efc1916b7410c1cab07454b2
Eryk Kopczyński <eryxyre@gmail.com>
2017-09-30 16:37:29 +0200

    changes to easy.cpp

--- course/easy.cpp
+++ course/easy.cpp
@@ -10,11 +10,25 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <map>
+#include <memory>
 
 namespace easy {
   using std::cout;
   using std::cin;
   using std::endl;
+  using std::make_pair;
+  using std::pair;
+  using std::ostream;
+  using std::unique_ptr;
+  using std::make_unique;
+  using std::shared_ptr;
+  using std::make_shared;
+  using std::weak_ptr;
+
   
   // character strings
   // C: char* s;
@@ -37,6 +51,9 @@ namespace easy {
   // C++: std::array<int, 20> a;
   // Easy: array<int, 20> a;
   using std::array;
+  using std::vector;
+  using std::deque;
+  using std::map;
   
   // writing and reading
   // C: printf("Czesc %s\n", kto);
@@ -45,9 +62,48 @@ namespace easy {
   // Easy: writeln('Czesc ', kto);
 
   void fwrite(std::ostream& os) {}
+  template<class T> void fwriterange(std::ostream& os, const T& x);
+  template<class T, class... TT> void fwrite(std::ostream& os, const T& x, TT... tt);
 
+  template<class T> void fwrite(std::ostream& os, const T& x) {
+    os << x;
+    };
+    
+  template<class T, class U> void fwrite(std::ostream& os, const std::pair<T,U>& p) {
+    fwrite(os, '(', p.first, ',', p.second, ')');
+    }
+  
+  template<class T> void fwrite(std::ostream& os, const vector<T>& x) {
+    fwriterange(os, x);
+    };
+    
+  template<class T, class U> void fwrite(std::ostream& os, const map<T,U>& x) {
+    fwriterange(os, x);
+    };
+    
+  template<class T> void fwrite(std::ostream& os, const deque<T>& x) {
+    fwriterange(os, x);
+    };
+    
+  template<class T, int U> void fwrite(std::ostream& os, const array<T,U>& x) {
+    fwriterange(os, x);
+    };
+  
+  template<class T> void fwriterange(std::ostream& os, const T& x) {
+    fwrite(os, "{"); 
+    bool first = true;
+    for(auto& el: x) { 
+      if(first) 
+        first = false;
+      else 
+        fwrite(os, ',');
+      fwrite(os, el);
+      }      
+    fwrite(os, '}');
+    };
+  
   template<class T, class... TT> void fwrite(std::ostream& os, const T& x, TT... tt) {
-    os << x; fwrite(os, tt...);
+    fwrite(os, x); fwrite(os, tt...);
     };
     
   template<class... TT> void fwriteln(std::ostream& os, TT... tt) {
@@ -65,5 +121,52 @@ namespace easy {
   template<class... TT> void write(TT... tt) { fwrite(cout, tt...);  };
 
   template<class... TT> void read(TT&... tt) { fread(cin, tt...);  };
-    
+  
+  // a helper
+  template <class T, class U> struct iterator_pair {
+    T t;
+    U u;
+    iterator_pair(T _t, U _u) : t(_t), u(_u) {} 
+    T& begin() { return t; }
+    U& end() { return u; }
+    const T& begin() const { return t; }
+    const U& end() const { return u; }
+   };
+
+  template <class T, class U> iterator_pair<T,U> make_iterator_pair(T t, U u) { return iterator_pair<T,U>(t,u); }
+  
+  struct iterator_int {
+    int value;
+    const int& operator*() { return value; }
+    iterator_int(int v) : value(v) {}
+    };
+  
+  inline bool operator < (const iterator_int& i1, const iterator_int& i2) { return i1.value < i2.value; }
+  inline bool operator > (const iterator_int& i1, const iterator_int& i2) { return i1.value > i2.value; }
+  inline bool operator <= (const iterator_int& i1, const iterator_int& i2) { return i1.value <= i2.value; }
+  inline bool operator >= (const iterator_int& i1, const iterator_int& i2) { return i1.value >= i2.value; }
+  inline bool operator == (const iterator_int& i1, const iterator_int& i2) { return i1.value == i2.value; }
+  inline bool operator != (const iterator_int& i1, const iterator_int& i2) { return i1.value != i2.value; }
+  inline bool operator ++ (iterator_int& i) { i.value++; }
+  
+  inline iterator_pair<iterator_int, iterator_int> ints(int to) { return make_iterator_pair(iterator_int(0), iterator_int(to)); }
+  inline iterator_pair<iterator_int, iterator_int> ints(int from, int to) { return make_iterator_pair(iterator_int(from), iterator_int(to)); }
+  
+  template<class T, class U, class... V> void sort(iterator_pair<T,U> p, V... v) { return std::sort(p.begin(), p.end(), v...); }
+  template<class T, class U, class... V> void reverse(iterator_pair<T,U> p, V... v) { return std::reverse(p.begin(), p.end(), v...); }
+  
+  template<class T> iterator_pair<iterator_int, iterator_int> indices(const vector<T>& v) { return ints(v.size()); }
+  template<class T> iterator_pair<iterator_int, iterator_int> indices(const deque<T>& v) { return ints(v.size()); }
+  template<class T, int U> iterator_pair<iterator_int, iterator_int> indices(const array<T,U>& v) { return ints(v.size()); }
+
+  // copied from: https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
+  template<typename ... Args>
+  string format( const std::string& format, Args ... args )
+  {
+    size_t size = snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
+    unique_ptr<char[]> buf( new char[ size ] ); 
+    snprintf( buf.get(), size, format.c_str(), args ... );
+    return string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
+    }
+     
   };
