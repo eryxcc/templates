72d0ab46dfc0cb96a9c42689fb8d74cb6f0486b3
Eryk Kopczyński <eryxyre@gmail.com>
2017-09-30 16:38:09 +0200

    a sample for LaTeX output

---
+++ sample/text-latex.cpp
@@ -0,0 +1,29 @@
+#define UTTF
+#define USDL
+#define UGFX
+#define UGD
+
+#include "../libs/graph2.h"
+
+int main() {
+  style b(0xFF000000, 0x80000000, 1);
+  pic p;
+  
+  p += line(b, vec(0,0), vec(200,0));
+  p += line(b, vec(200,0), vec(200,200));
+  p += line(b, vec(200,200), vec(0,200));
+  p += line(b, vec(0,200), vec(0,0));
+  p += line(b, vec(100, 200), vec(100, 0));
+  p += line(b, vec(200, 100), vec(0, 100));
+  p += line(b, vec(200, 120), vec(0, 120));
+  
+  font ff = makefont("DejaVuSans-Bold.ttf", "latex");
+  
+  p += text(b, ff, vec(100, 100), topleft, 20, "hello");
+  p += text(b, ff, vec(100, 100), topright, 20, "hey");
+  
+  svg_normalizer s("text-latex.svg");
+  s += p;
+  
+  return 0;
+  }
